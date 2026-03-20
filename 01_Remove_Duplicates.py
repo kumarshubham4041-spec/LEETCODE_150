8a5e9123d6032848ea913409c4a210d4dff748d3
Shubham Kumar <kumarshubham4041@gmail.com>
2026-03-20 22:44:40 +0530

    Update 01_Remove_Duplicates.py

--- 01_Remove_Duplicates.py
+++ 01_Remove_Duplicates.py
@@ -1,5 +1,5 @@
 # Question: In this question we have to ramove dduplicates from sorted array in place such that any elements appears only twice and the order of the elements is maintained.
-# Approach : In this question , i have applied two pointer technique which move in the same direction . Firstly i compared present element with the element two places before. if it matches, then this element has appeared thrice so it is replaced by the next new element.
+# Approach : I have applied two pointer technique which move in the same direction . Firstly i compared present element with the element two places before. if it matches, then this element has appeared thrice so it is replaced by the next new element.
 #Time Complexity : O(n)
 # Space Complexity : O(1)
 
