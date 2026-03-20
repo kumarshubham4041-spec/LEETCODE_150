dad4b5eeea68001dc03f79a27f2d6d1e71d13f2b
Shubham Kumar <kumarshubham4041@gmail.com>
2026-03-20 22:45:39 +0530

    Update 02_Majority_Element.py

--- 02_Majority_Element.py
+++ 02_Majority_Element.py
@@ -1,5 +1,5 @@
 # Question : In this question , we have to return the majority element which appears more than [n/2] times in an array of size n.
-# Approach : In this question i have maintained two variables one for majority element and one for keeping its count . whenever same element appears its count increases and when other element appears , previous elements' count decreases . when count becomes 0 majority element changes and its count increase . This process continues until majority element is found.
+# Approach : I have maintained two variables one for majority element and one for keeping its count . whenever same element appears its count increases and when other element appears , previous elements' count decreases . when count becomes 0 majority element changes and its count increase . This process continues until majority element is found.
 # Time Complexity : O(n)
 # Space Complexity : O(1)
 
