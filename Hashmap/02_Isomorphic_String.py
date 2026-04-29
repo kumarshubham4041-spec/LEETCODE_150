e4a00f3d15ff27f49db499473026f66f3dd3d661
Shubham Kumar <kumarshubham4041@gmail.com>
2026-04-30 00:00:22 +0530

    Update 02_Isomorphic_String.py

--- Hashmap/02_Isomorphic_String.py
+++ Hashmap/02_Isomorphic_String.py
@@ -1,5 +1,6 @@
 # Question : Given two strings s and t, determine if they are isomorphic.Two strings s and t are isomorphic if the characters in s can be replaced to get t.All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
-
+# Time Complexity : O(n)
+# Space Complexity : O(n)
 
     def isIsomorphic(self, s: str, t: str) -> bool:
         if len(s)!= len(t):
